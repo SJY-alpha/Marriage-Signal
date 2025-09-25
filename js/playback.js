9b9b7933aa3cbcfd20882cdfd5d5d57bcb6eea32
SJY-alpha <ms1797@naver.com>
2025-09-25 21:02:46 +0900

    Create playback.js

---
+++ js/playback.js
@@ -0,0 +1,31 @@
+import { state } from './app.js';
+import { DOM_ELEMENTS } from './config.js';
+import { showToast } from './ui.js';
+
+export function stopSinglePlayback() {
+    // ... 원본 stopSinglePlayback 함수 내용
+}
+
+export async function playSingleDialogue(dialogue, controlsElement) {
+    // ... 원본 playSingleDialogue 함수 내용
+}
+
+export function playTimeline() {
+    // ... 원본 playTimeline 함수 내용
+}
+
+export function pauseTimeline() {
+    // ... 원본 pauseTimeline 함수 내용
+}
+
+export function stopTimeline() {
+    // ... 원본 stopTimeline 함수 내용
+}
+
+function scheduleAudio() {
+    // ... 원본 scheduleAudio 함수 내용
+}
+
+function updatePlaybackFrame() {
+    // ... 원본 updatePlaybackFrame 함수 내용
+}
